22d904e71982a318ac0c1667a62a758b3095c7e2
Ruan Eloy <ruan.eloy@thoughtworks.com>
2023-03-01 14:23:35 -0300

    add tree implementation

---
+++ src/tree/index.test.ts
@@ -0,0 +1,33 @@
+import {beforeEach, describe, expect, it} from '@jest/globals';
+import Tree from './index';
+
+describe('Tree', () => { 
+  it('should create a tree with one node', () => {
+    const values = [1];
+    const tree = new Tree(values);
+    expect(tree.root?.value).toBe(1);
+  });
+
+  it('should have the correct children', () => {
+    const values = [1, 2, 3, 4, 5];
+    const tree = new Tree(values);
+    expect(tree.root?.value).toBe(1);
+    // root's children
+    expect(tree.root?.left?.value).toBe(2);
+    expect(tree.root?.right?.value).toBe(3);
+    // children of node 2
+    expect(tree.root?.left?.left?.value).toBe(4);
+    expect(tree.root?.left?.right?.value).toBe(5);
+  });
+
+  it('should get the correct tree hight', () => {
+    let tree = new Tree([]);
+    expect(tree.getHight()).toBe(0);
+    tree = new Tree([1]);
+    expect(tree.getHight()).toBe(1);
+    tree = new Tree([1, 2]);
+    expect(tree.getHight()).toBe(2);
+    tree = new Tree([1, 2, 3, 4, 5, 6, 7, 8]);
+    expect(tree.getHight()).toBe(4);
+  });
+})
---
+++ src/tree/index.ts
@@ -0,0 +1,60 @@
+class TreeNode {
+  constructor(
+    public value: number,
+    public left: TreeNode | null,
+    public right: TreeNode | null,
+  ) {}
+
+  isLeftEmpty() {
+    return this.left === null;
+  }
+
+  toString() {
+    return this.value.toString();
+  }
+
+}
+
+export default class Tree {
+  public root: TreeNode | null = null;
+
+  constructor(values: Array<number>) {
+    if (values.length > 0) this.fillTree(values);
+  }
+
+  private fillTree(values: Array<number>) {
+    const nodes: TreeNode[] = [];
+    this.root = new TreeNode(values[0], null, null);
+    nodes.push(this.root);
+
+    for (let i = 1; i < values.length; i++) {
+      const newNode = new TreeNode(values[i], null, null);
+      const parentIndex = this.getParentIndex(i);
+      const parentNode = nodes[parentIndex];
+      if (parentNode.isLeftEmpty()) {
+        parentNode.left = newNode;
+      } else {
+        parentNode.right = newNode;
+      }
+      nodes.push(newNode);
+    }
+  }
+
+  private getParentIndex(nodeIndex: number): number {
+    const isEven = nodeIndex % 2 == 0;
+    return isEven ? (nodeIndex / 2) - 1 : (nodeIndex - 1) / 2;
+  }
+
+  getHight() {
+    if(!this.root) return 0;
+
+    let hight = 0;
+    let currentNode: TreeNode | null = this.root;
+    while(currentNode) {
+      hight++;
+      currentNode = currentNode?.left;
+    }
+    return hight;
+  }
+
+}
